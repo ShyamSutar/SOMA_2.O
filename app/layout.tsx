aa4805f81e54f96a1a8a04faceffda112bffee12
shyamsutar <shyamsutar629@gmail.com>
2023-08-21 19:51:46 +0530

    created reusable Modal / Button Components

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Nunito } from "next/font/google"
 import Navbar from './components/Navbar/Navbar'
+import Modal from './components/modals/Modal'
 
 const font = Nunito({
   subsets: ["latin"]
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <Modal isOpen title='hello'/>  {/* same as isOpen=true */}
         <Navbar/>
         {children}
         </body>
