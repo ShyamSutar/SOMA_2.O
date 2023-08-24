e49bfd19050cf3a8a5aabd902f4617d6b0cfd2cd
shyamsutar <shyamsutar629@gmail.com>
2023-08-24 17:34:23 +0530

    React-Hook-Form

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -1,6 +1,36 @@
-import React from 'react'
+"use client"
+
+import { AiFillGithub } from "react-icons/ai"
+import {FcGoogle} from "react-icons/fc"
+import { useCallback, useState } from "react"
+import {
+  FieldValues,
+  SubmitHandler,
+  useForm
+} from "react-hook-form"
+
+import useRegisterModal from "@/app/hooks/useRegisterModal"
 
 const RegisterModal = () => {
+
+  const RegisterModal = useRegisterModal()
+  const [isLoading, setIsLoading] = useState(false)
+
+  const{
+    register,
+    handleSubmit,
+    formState:{
+      errors,
+    }
+  } = useForm<FieldValues>({
+  defaultValues:{
+      name: '',
+      email: '',
+      password: '' //1:08:12
+  
+  } 
+      })
+
   return (
     <div>
         
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Nunito } from "next/font/google"
 import Navbar from './components/Navbar/Navbar'
-import Modal from './components/modals/Modal'
+import RegisterModal from './components/modals/RegisterModal'
 
 const font = Nunito({
   subsets: ["latin"]
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        
+        <RegisterModal/>
         <Navbar/>
         {children}
         </body>
